8baf8e23ea33ce6f2d44316c98c53b5e8096726d
zayan <ask2zayan@gmail.com>
2026-09-20 18:49:22 +0500

    Add files via upload

---
+++ football.js
@@ -0,0 +1,19 @@
+
+var marks = prompt("give us marks");
+
+
+if(marks >= 90){
+    console.log("Grade A");
+    
+}else if(marks >= 80){
+    console.log("Grade B");
+    
+}else if(marks >= 70){
+    console.log("Grade C");
+    
+}else if(marks >= 60){
+    console.log("Grade D");
+}else{
+    console.log("You Failed!");
+    
+}
